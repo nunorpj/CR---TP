3308da384461935a35e5bd71e8ba73e35bf75d03
Nuno Rafael <nunorpj@gmail.com>
2018-05-27 15:53:07 +0100

    added: Exame

---
+++ src/main/java/com/sample/Exame.java
@@ -0,0 +1,34 @@
+package com.sample;
+
+public class Exame {
+
+	private int id;
+	private String Designacao; //nome do exame
+	private String Resultado; //resultado do exame
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDesignacao() {
+		return Designacao;
+	}
+	public void setDesignacao(String designacao) {
+		Designacao = designacao;
+	}
+	public String getResultado() {
+		return Resultado;
+	}
+	public void setResultado(String resultado) {
+		Resultado = resultado;
+	}
+	public Exame(int id, String designacao, String resultado) {
+		super();
+		this.id = id;
+		Designacao = designacao;
+		Resultado = resultado;
+	}
+	
+	
+}
