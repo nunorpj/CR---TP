9aad783654fc3a245aca1ee05b5a0511a3fcc81b
Nuno Rafael <nunorpj@gmail.com>
2018-05-27 15:53:30 +0100

    updated: Paciente

--- src/main/java/com/sample/Paciente.java
+++ src/main/java/com/sample/Paciente.java
@@ -7,9 +7,15 @@ public class Paciente {
 	private int Id; //id do paciente(atribuir automaticamente)
 	private String Nome; //nome do paciente
 	private Vector<Nodule> Nodolos; //vetor de modulos do paciente
-	private int HCC; // se tiver HCC add id do mesmo, senao 0;
-
+	private int Id_Exame; // id do exame // 0 se nao tiver exame
 	
+	
+	public int getId_Exame() {
+		return Id_Exame;
+	}
+	public void setId_Exame(int id_Exame) {
+		Id_Exame = id_Exame;
+	}
 	public int getId() {
 		return Id;
 	}
@@ -29,20 +35,14 @@ public class Paciente {
 	public void setNodolos(Vector<Nodule> nodolos) {
 		Nodolos = nodolos;
 	}
-	public int getHCC() {
-		return HCC;
-	}
-	public void setHCC(int hCC) {
-		HCC = hCC;
-	}
-	
+
 	
 	public Paciente(int id, String nome) {
 		super();
 		Id = id;
 		Nome = nome;
 		Nodolos = new Vector<>();
-		HCC = 0;
+		Id_Exame = 0;
 	}
 
 }
