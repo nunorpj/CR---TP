d2e442274f0d602e1f22a6be5fe162407cf09a4e
Nuno Rafael <nunorpj@gmail.com>
2018-06-12 15:50:58 +0100

    added: Sintomas de perigo

--- src/main/java/com/sample/Paciente.java
+++ src/main/java/com/sample/Paciente.java
@@ -6,9 +6,15 @@ public class Paciente {
 
 	private String Nome; //nome do paciente
 	private Vector<Nodule> Nodolos; //vetor de modulos do paciente
-	private HCC hcc;
+	private boolean cirrose;
+	private char estadio; //0 = nada, A,B,C,D
+	private boolean VHB;
+	private boolean hepatite_ativa;
+	private boolean historicoCHC; //historico de familiar com chc
+	private boolean HepatiteCronica;
+	private boolean fibroseHipaticaAvançada;
+	private boolean aptoParaViginalancia;
 	
-
 	public String getNome() {
 		return Nome;
 	}
@@ -24,18 +30,87 @@ public class Paciente {
 		Nodolos.add(nodolos);
 	}
 
+	public boolean isCirrose() {
+		return cirrose;
+	}
+
+	public void setCirrose(boolean cirrose) {
+		this.cirrose = cirrose;
+	}
+
+	public char getEstadio() {
+		return estadio;
+	}
+
+	public void setEstadio(char estadio) {
+		this.estadio = estadio;
+	}
+
+	public boolean isVHB() {
+		return VHB;
+	}
+
+	public void setVHB(boolean vHB) {
+		VHB = vHB;
+	}
+
+	public boolean isHepatite_ativa() {
+		return hepatite_ativa;
+	}
+
+	public void setHepatite_ativa(boolean hepatite_ativa) {
+		this.hepatite_ativa = hepatite_ativa;
+	}
+
+	public boolean isHistoricoCHC() {
+		return historicoCHC;
+	}
+
+	public void setHistoricoCHC(boolean historicoCHC) {
+		this.historicoCHC = historicoCHC;
+	}
+
+	public boolean isHepatiteCronica() {
+		return HepatiteCronica;
+	}
+
+	public void setHepatiteCronica(boolean hepatiteCronica) {
+		HepatiteCronica = hepatiteCronica;
+	}
+
+	public boolean isFibroseHipaticaAvançada() {
+		return fibroseHipaticaAvançada;
+	}
+
+	public void setFibroseHipaticaAvançada(boolean fibroseHipaticaAvançada) {
+		this.fibroseHipaticaAvançada = fibroseHipaticaAvançada;
+	}
+
 	
-	public Paciente( String nome) {
+	
+	public boolean isAptoParaViginalancia() {
+		return aptoParaViginalancia;
+	}
+
+	public void setAptoParaViginalancia(boolean aptoParaViginalancia) {
+		this.aptoParaViginalancia = aptoParaViginalancia;
+	}
+
+	public Paciente(String nome, boolean cirrose, char estadio, boolean vHB, boolean hepatite_ativa,
+			boolean historicoCHC, boolean hepatiteCronica, boolean fibroseHipaticaAvançada) {
 		super();
-		Nome = nome;
-		Nodolos = new Vector<>();
-		setHcc(null);
+		this.Nome = nome;
+		this.cirrose = cirrose;
+		this.estadio = estadio;
+		this.VHB = vHB;
+		this.hepatite_ativa = hepatite_ativa;
+		this.historicoCHC = historicoCHC;
+		this.HepatiteCronica = hepatiteCronica;
+		this.fibroseHipaticaAvançada = fibroseHipaticaAvançada;
+		this.aptoParaViginalancia = false;
 	}
-	public HCC getHcc() {
-		return hcc;
-	}
-	public void setHcc(HCC hcc) {
-		this.hcc = hcc;
-	}
+
+	
+
 
 }
